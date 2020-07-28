53483772d34add79e8a4274d2b7658db8dcfd576
Alon Hasson <alonjxui@gmail.com>
2020-07-28 15:09:47 +0300

    Here we go! v0.1

---
+++ Bulk-File-Renaming.py
@@ -0,0 +1,80 @@
+import argparse
+from pathlib import Path
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Use this script for bulk file renaming, I encourage you to read the '
+                                                 'HELP menu in order to be familiar with the different options you have'
+                                                 '. *** Python3.4+ required.')
+    parser.add_argument("src")
+    parser.add_argument('-d', '--dst', help='Destination folder files are going to be move to.', nargs='?', const='True')
+    parser.add_argument('-p', '--pattern', help='Use pattern to filter the files that will be effected, \n'
+                                                "e.g: \"*.py$\" -> effects all files with \"py\" suffix.")
+    parser.add_argument('-r', '--recursive', help='Effects all files including those located in sub-directories.',
+                        nargs='?', const='True')
+    parser.add_argument('-n', '--new-name', help='set new name for matching files with an id (e.g xxx-1, xxx-2).',
+                        nargs='?', const='True')
+    parser.add_argument('-c', '--handle-collisions', help='Automatically handle name collision that might occur when '
+                                                          ' moving files without defining "-n" arg', nargs='?',
+                        const='True')
+
+    args = parser.parse_args()
+    assert args.new_name or args.dst, "You have to use at least one of the arguments (--dst, --new-name)"
+    return args
+
+
+def rename_only(path, glob_str, dst_name):
+    previous_parent = None
+    for f in path.glob(glob_str):
+        if previous_parent != f.parent:
+            previous_parent = f.parent
+            i = 0
+        if not f.is_dir():
+            name = "%s-%s" % (dst_name, i)
+            f.rename(f.with_name(name).with_suffix(f.suffix))
+            i += 1
+
+
+def move_only(path, glob_str, dst, handle_collisions=True):
+    destination_path = Path(dst)
+    for f in path.glob(glob_str):
+        if not f.is_dir():
+            new_path = (destination_path / f.name)
+            if handle_collisions:
+                i = 0
+                while new_path.exists():
+                    i += 1
+                    new_name = "%s-%s" % (f.stem, i)
+                    new_path = new_path.with_name(new_name).with_suffix(new_path.suffix)
+            f.rename(new_path)
+
+
+def move_and_rename(path, glob_str, dst, dst_name):
+    destination_path = Path(dst)
+    i = 0
+
+    for f in path.glob(glob_str):
+        if not f.is_dir():
+            name = "%s-%s" % (dst_name, i)
+            f.rename((destination_path / name).with_suffix(f.suffix))
+            i += 1
+
+
+def main():
+    args = parse_args()
+    path = Path(args.src)
+    glob_str = args.pattern if args.pattern else "*"
+    if args.recursive:
+        glob_str = "**/" + glob_str
+
+    if args.dst:
+        if args.new_name:
+            move_and_rename(path, glob_str, args.dst, args.new_name)
+        else:
+            move_only(path, glob_str, args.dst, args.new_name, args.handle_collisions)
+    elif args.new_name:
+        rename_only(path, glob_str, args.new_name)
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Bulk-File-Renaming
+### Background
+There are a lot of different use cases for renaming a bulk of files,
+from organizing your Machine Learning datasets, simply locate and move specific types of files, etc.
+
+I found many programs/scripts that are doing that, but their code is more difficult as it should be,
+most of the time this is caused by feature fatigue.
+
+So this simple script is here to solve it, easy to follow, and change it for your usages.
+
+**Feel free to open an issue for any bug you find or another feature you think might feet.**
+
+
+#### **HELP menu**
+usage: `Bulk-File-Renaming.py [-h] [-d [DST]] [-p PATTERN] [-r [RECURSIVE]] [-n [NEW_NAME]] [-c [HANDLE_COLLISIONS]]
+                             src`
+
+
+positional arguments:
+  src
+
+optional arguments:
+  * -h, --help  show this help message and exit
+  * -d, --dst Destination folder files are going to be move to.
+  * -p, --pattern Use pattern to filter the files that will be effected, e.g: "*.py$" -> effects all files with "py" suffix.
+  * -r, --recursive Effects all files including those located in sub-directories.
+  * -n, --new-name set new name for matching files with an id (e.g xxx-1, xxx-2).
+  * -c, --handle-collisions Automatically handle name collision that might occur when moving files without defining "-n" arg
+##### **Requirements**
+
+* Python 3.4+
